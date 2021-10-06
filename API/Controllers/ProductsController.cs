94eb111b4e9c8a4bcd8154b3b94c1545e1ee1119
Yathusanan Yokarajah <imyathusanan@gmail.com>
2021-10-06 19:50:16 +0530

    InitialCommit

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,38 @@
+﻿using API.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _context;
+
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await _context.Products.ToListAsync();
+
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public string GetProduct(int id)
+        {
+            return "single Product";
+        }
+    }
+}
