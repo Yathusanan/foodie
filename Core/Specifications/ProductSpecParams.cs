f0f81777fdc9993bcf1ac91ff5e13e28138dbf3c
Yathusanan Yokarajah <imyathusanan@gmail.com>
2021-11-06 12:50:28 +0530

    PaginationFunctionalityAddedToApi

---
+++ Core/Specifications/ProductSpecParams.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Specifications
+{
+    public class ProductSpecParams
+    {
+        private const int _maxPageSize = 50;
+        public int PageIndex { get; set; } = 1;
+
+        private int _pageSize = 6;
+        public int PageSize
+        {
+            get => _pageSize;
+            set => _pageSize = (value > _maxPageSize) ? _maxPageSize : value;
+        }
+        public int? BrandId { get; set; }
+        public int? TypeId { get; set; }
+        public string Sort { get; set; }
+    }
+}
--- Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
+++ Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
@@ -7,19 +7,20 @@ namespace Core.Specifications
 {
     public class ProductsWithTypesAndBrandsSpecification : BaseSepcification<Product>
     {
-        public ProductsWithTypesAndBrandsSpecification(string sort, int? brandId, int? typeId)
+        public ProductsWithTypesAndBrandsSpecification(ProductSpecParams productSpecParams)
             :base(x =>
-                  (!brandId.HasValue || x.ProductBrandId == brandId) &&
-                  (!typeId.HasValue || x.ProductTypeId == typeId)
+                  (!productSpecParams.BrandId.HasValue || x.ProductBrandId == productSpecParams.BrandId) &&
+                  (!productSpecParams.TypeId.HasValue || x.ProductTypeId == productSpecParams.TypeId)
             )
         {
             AddIncludes(x => x.ProductType);
             AddIncludes(x => x.ProductBrand);
             AddOrderBy(x => x.Name);
+            ApplyPaging(productSpecParams.PageSize * (productSpecParams.PageIndex - 1), productSpecParams.PageSize);
 
-            if (!string.IsNullOrEmpty(sort))
+            if (!string.IsNullOrEmpty(productSpecParams.Sort))
             {
-                switch (sort)
+                switch (productSpecParams.Sort)
                 {
                     case "priceAsc":
                         AddOrderBy(p => p.Price);
