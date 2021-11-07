b17513e4aeac1c487d9d9469cc797e404fdc60cd
Yathusanan Yokarajah <imyathusanan@gmail.com>
2021-11-07 21:29:00 +0530

    NavBarCreatedInClient

--- API/Startup.cs
+++ API/Startup.cs
@@ -33,7 +33,7 @@ namespace API
             services.AddCors(opt => {
                 opt.AddPolicy("CorsPolicy", policy =>
                 {
-                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:44398");
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
                 });
             });
         }
