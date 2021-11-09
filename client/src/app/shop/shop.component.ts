24f17a1c2278632d5cc1015efe1fbf15689bc90d
Yathusanan Yokarajah <imyathusanan@gmail.com>
2021-11-09 10:58:08 +0530

    FilteringFunctionalityaddedToClientApp

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -1,3 +1,5 @@
+import { IType } from './../shared/models/productType';
+import { IBrand } from './../shared/models/brand';
 import { ShopService } from './shop.service';
 import { IProduct } from '../shared/models/product';
 import { Component, OnInit } from '@angular/core';
@@ -10,15 +12,51 @@ import { Component, OnInit } from '@angular/core';
 export class ShopComponent implements OnInit {
 
   products: IProduct[] = [];
+  brands: IBrand[] = [];
+  types: IType[] = [];
+  brandIdSelected: number = 0;
+  typeIdSelected: number = 0;
 
   constructor(private shopService: ShopService) { }
 
   ngOnInit(): void {
-    this.shopService.getProducts().subscribe((response: any) => {
+    this.getProducts();
+    this.getBrands();
+    this.getTypes();
+  }
+
+  getProducts() {
+    this.shopService.getProducts(this.brandIdSelected, this.typeIdSelected).subscribe((response: any) => {
       this.products = response.data;
     }, error => {
       console.log(error);
     });
+  }
+
+  getBrands() {
+    this.shopService.getBrands().subscribe((response: any) => {
+      this.brands = [{id: 0, name: 'All'}, ...response];
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  getTypes() {
+    this.shopService.getTypes().subscribe((response: any) => {
+      this.types = [{id: 0, name: 'All'}, ...response];
+    }, error => {
+      console.log(error)
+    });
+  }
+
+  onBrandSelected(brandId: number) {
+    this.brandIdSelected = brandId;
+    this.getProducts();
+  }
+
+  onTypeSelected(typeId: number) {
+    this.typeIdSelected = typeId;
+    this.getProducts();
   }
 
 }
--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -1,16 +1,40 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShopService {
 
-  baseUrl = "https://localhost:44398/api/";
+  private baseUrl = "https://localhost:44398/api/";
 
   constructor(private http: HttpClient) { }
 
-  getProducts() {
-    return this.http.get(this.baseUrl + 'products');
+  getProducts(brandId?: number, typeId?: number) {
+    let params = new HttpParams();
+
+    if (brandId) {
+      params = params.append('brandId', brandId.toString());
+    }
+
+    if (typeId) {
+      params = params.append('typeId', typeId.toString());
+    }
+
+    return this.http.get(this.baseUrl + 'products', {observe: 'response', params})
+                    .pipe(
+                      map( response => {
+                        return response.body;
+                      })
+                    );
+  }
+
+  getBrands() {
+    return this.http.get(this.baseUrl + 'products/brands');
+  }
+
+  getTypes() {
+    return this.http.get(this.baseUrl + 'products/types');
   }
 }
