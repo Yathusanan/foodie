bf33a5cb840243b668c94d0edde1ee8b782c33aa
Yathusanan Yokarajah <imyathusanan@gmail.com>
2022-04-03 12:33:30 +0530

    loader added

--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -1,6 +1,8 @@
 import { IProduct } from 'src/app/shared/models/product';
 import { ShopService } from './../shop.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { BreadcrumbService } from 'xng-breadcrumb';
 
 @Component({
   selector: 'app-product-details',
@@ -11,15 +13,19 @@ export class ProductDetailsComponent implements OnInit {
 
   product: any;
 
-  constructor(private shopService: ShopService) { }
+  constructor(private shopService: ShopService, private activateRoute: ActivatedRoute, private bcService: BreadcrumbService) { 
+    this.bcService.set('@productDetails', '');
+  }
+  
 
   ngOnInit(): void {
     this.loadProduct();
   }
 
   loadProduct() {
-    this.shopService.getProduct(1).subscribe(product => {
+    this.shopService.getProduct(+this.activateRoute.snapshot.paramMap.get('id')).subscribe((product: any) => {
       this.product = product;
+      this.bcService.set('@productDetails', product.name);
     }, error => {
       console.log(error);
     });
